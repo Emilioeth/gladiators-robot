eaab5f8c2f6c1f02e62e6ac05b40a9b416872b90
Emilio <emiliothomasking@gmail.com>
2022-05-24 21:45:59 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,8 +70,18 @@ var fight = function(enemyName) {
     } // end of while loop
   }; // end of fight function
 
-for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+
+
+    for (var i = 0; i < enemyNames.length; i++) {
+
+        if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );         
+
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
 }
